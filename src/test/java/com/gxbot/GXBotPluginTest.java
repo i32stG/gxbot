4d269f0bebafef9d6e179e7320d7ebc6a2089797
George <george_b_93@hotmail.co.uk>
2024-04-12 15:39:32 +0100

    Plugins renamed

--- src/test/java/com/gxbot/GXBotPluginTest.java
+++ src/test/java/com/gxbot/GXBotPluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.gxbot;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class GXBotPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(GXBotPlugin.class);
 		RuneLite.main(args);
 	}
 }
